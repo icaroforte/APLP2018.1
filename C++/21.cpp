a8a15da064f460c2aee7138f8588d04150ad603c
Ícaro Forte <icaro_forte@hotmail.com>
2018-05-07 11:01:05 -0300

    Função adicionarCartaJogador agora adiciona carta sem duplicação, e remove a carta do baralho principal

--- C++/21.cpp
+++ C++/21.cpp
@@ -16,6 +16,7 @@ Funções
 int somaPontuacao(string baralhoASomar[][2]);
 void adicionaCartaJogador(string jogador[][2]);
 void exibeBaralho(string baralhoAExibir[][2]);
+int geraValorAleatorio();
 
 /*
 Variáveis globais
@@ -27,58 +28,58 @@ Ex: baralho [0][0] - informa "Ás de copas" --------- baralho [0][1] - informa o
 Obs: Sempre que o segundo índice for zero representa o nome da carta e sempre que o segundo índice for 1 representa o valor.
 */
 std::string baralho[52][2] = {
-    {"Ás de copas","1"},
-    {"Ás de espadas","1"},
-    {"Ás de ouro","1"},
-    {"Ás de paus","1"},
-    {"Dois de copas","2"},
-    {"Dois de espadas","2"},
-    {"Dois de ouro","2"},
-    {"Dois de paus","2"},
-    {"Três de copas","3"},
-    {"Três de espadas","3"},
-    {"Três de ouro","3"},
-    {"Três de paus","3"},
-    {"Quatro de copas","4"},
-    {"Quatro de espadas","4"},
-    {"Quatro de ouro","4"},
-    {"Quatro de paus","4"},
-    {"Cinco de copas","5"},
-    {"Cinco de espadas","5"},
-    {"Cinco de ouro","5"},
-    {"Cinco de paus","5"},
-    {"Seis de copas","6"},
-    {"Seis de espadas","6"},
-    {"Seis de ouro","6"},
-    {"Seis de paus","6"},
-    {"Sete de copas","7"},
-    {"Sete de espadas","7"},
-    {"Sete de ouro","7"},
-    {"Sete de paus","7"},
-    {"Oito de copas","8"},
-    {"Oito de espadas","8"},
-    {"Oito de ouro","8"},
-    {"Oito de paus","8"},
-    {"Nove de copas","9"},
-    {"Nove de espadas","9"},
-    {"Nove de ouro","9"},
-    {"Nove de paus","9"},
-    {"Dez de copas","10"},
-    {"Dez de espadas","10"},
-    {"Dez de ouro","10"},
-    {"Dez de paus","10"},
-    {"Valete de copas","10"},
-    {"Valete de espadas","10"},
-    {"Valete de ouro","10"},
-    {"Valete de paus","10"},
-    {"Dama de copas","10"},
-    {"Dama de espadas","10"},
-    {"Dama de ouro","10"},
-    {"Dama de paus","10"},
-    {"Rei de copas","10"},
-    {"Rei de espadas","10"},
-    {"Rei de ouro","10"},
-    {"Rei de paus","10"}
+    {"Ás de copas","1"},//0
+    {"Ás de espadas","1"},//1
+    {"Ás de ouro","1"},//2
+    {"Ás de paus","1"},//3
+    {"Dois de copas","2"},//4
+    {"Dois de espadas","2"},//5
+    {"Dois de ouro","2"},//6
+    {"Dois de paus","2"},//7
+    {"Três de copas","3"},//8
+    {"Três de espadas","3"},//9
+    {"Três de ouro","3"},//10
+    {"Três de paus","3"},//11
+    {"Quatro de copas","4"},//12
+    {"Quatro de espadas","4"},//13
+    {"Quatro de ouro","4"},//14
+    {"Quatro de paus","4"},//15
+    {"Cinco de copas","5"},//16
+    {"Cinco de espadas","5"},//17
+    {"Cinco de ouro","5"},//18
+    {"Cinco de paus","5"},//19
+    {"Seis de copas","6"},//20
+    {"Seis de espadas","6"},//21
+    {"Seis de ouro","6"},//22
+    {"Seis de paus","6"},//23
+    {"Sete de copas","7"},//24
+    {"Sete de espadas","7"},//25
+    {"Sete de ouro","7"},//26
+    {"Sete de paus","7"},//27
+    {"Oito de copas","8"},//28
+    {"Oito de espadas","8"},//29
+    {"Oito de ouro","8"},//30
+    {"Oito de paus","8"},//31
+    {"Nove de copas","9"},//32
+    {"Nove de espadas","9"},//33
+    {"Nove de ouro","9"},//34
+    {"Nove de paus","9"},//35
+    {"Dez de copas","10"},//36
+    {"Dez de espadas","10"},//37
+    {"Dez de ouro","10"},//38
+    {"Dez de paus","10"},//39
+    {"Valete de copas","10"},//40
+    {"Valete de espadas","10"},//41
+    {"Valete de ouro","10"},//42
+    {"Valete de paus","10"},//43
+    {"Dama de copas","10"},//44
+    {"Dama de espadas","10"},//45
+    {"Dama de ouro","10"},//46
+    {"Dama de paus","10"},//47
+    {"Rei de copas","10"},//48
+    {"Rei de espadas","10"},//49
+    {"Rei de ouro","10"},//50
+    {"Rei de paus","10"}//51
 };
 
 //Baralho do jogador Humano
@@ -119,7 +120,6 @@ adicionaCartaJogador(jogadorMaquina);
 adicionaCartaJogador(jogadorMaquina);
 
 
-
 //Atribuindo o resultado da função somaPontuacao a variável pontuação do jogador humano e máquina
 pontuacaoJogadorHumano = somaPontuacao(jogadorHumano);
 pontuacaoJogadorMaquina = somaPontuacao(jogadorMaquina);
@@ -132,8 +132,7 @@ cout <<"---------------------------------------------------" << endl;
 exibeBaralho(jogadorMaquina);
 cout <<"Valor de pontos do jogador máquina é: " << pontuacaoJogadorMaquina << endl;
 
-//baralho[50][0] = "";
-//baralho[50][1] = "";
+
 
 //exibeBaralho(baralho);
 
@@ -158,26 +157,42 @@ int somaPontuacao(string baralhoASomar[][2]){
     return totalPontos;
 }
 
-//Função incompleta que adiciona carta ao baralho de um jogador
-void adicionaCartaJogador(string jogador[][2]){
-//Gerando um índice aleatório
+//Função que retorna um valor aleatório entre [0,51]
+int geraValorAleatorio(){
     std::random_device rd;
     std::mt19937 rng(rd());
-    std::uniform_int_distribution<int> uni(0,52);
+    std::uniform_int_distribution<int> uni(0,51);
     auto random_integer = uni(rng);
-//exibe o inteiro aleatório gerado
-cout << "Inteiro aleatório gerado: " << random_integer << endl;
+    return random_integer;
+}
+
+
+//Função que adiciona carta ao baralho de um jogador sem repetição
+void adicionaCartaJogador(string jogador[][2]){
+
+//Gerando um índice aleatório
+    int valorAleatorio = geraValorAleatorio();
+//Este WHILE serve para que se a posição random no baralho principal esteja vazia (já tenha sido usada), ele gera um novo número random.
+    while(baralho[valorAleatorio][0] == ""){
+        valorAleatorio = geraValorAleatorio();
+    }
+//cout << "Inteiro aleatório gerado: " << valorAleatorio << endl;
 
     string nomeCarta;
     string valorCarta;
-
-    nomeCarta = baralho[random_integer][0];
-    valorCarta = baralho[random_integer][1];
-
+//Recebe o nome e o valor da carta do baralho principal;
+    nomeCarta = baralho[valorAleatorio][0];
+    valorCarta = baralho[valorAleatorio][1];
+/*Este FOR serve para atribuir o nome e o valor da carta do baralho a primeira posição vazia encontrada no
+baralho do jogador. Ao mesmo tempo, "deleta" a carta do baralho principal para que não possa ser usada mais de uma vez.
+*/
     for (int i=1; i<=52; i++){
         if(jogador[i][0] == "" & jogador[i][1] == ""){
             jogador[i][0] = nomeCarta;
             jogador[i][1] = valorCarta;
+            //"deletando" a carta do baralho principal já que ele agora pertence ao jogador
+            baralho[valorAleatorio][0] = "";
+            baralho[valorAleatorio][1] = "";
             return;
         }
     }
