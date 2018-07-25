cfc55ab745164d23c405563dad58ba6d03225949
Ícaro Forte <icaro_forte@hotmail.com>
2018-07-25 11:19:14 -0300

    Adicionando método que retorna o tamanho de uma lista

--- Prolog/21.pl
+++ Prolog/21.pl
@@ -19,15 +19,21 @@ executaMenu():-
     read(OPCAO),
     opcao(OPCAO).
 
+ 
+    
 opcao(1):-
     write("Dificuldade escolhida: Fácil"), nl,
-    BaralhoNomeCarta = ["Ás de copas", "Dois de ouro"],
-    BaralhoValorCarta = [1, 2],
+    
+    BaralhoNomeCarta = ["As de copas", "As de espadas", "As de ouro", "As de paus", "Dois de copas","Dois de espadas", "Dois de ouro", "Dois de paus", "Tres de copas", "Tres de espadas", "Tres de ouro", "Tres de paus", "Quatro de copas", "Quatro de espadas", "Quatro de ouro", "Quatro de paus", "Cinco de copas", "Cinco de espadas", "Cinco de ouro", "Cinco de paus", "Seis de copas", "Seis de espadas", "Seis de ouro", "Seis de paus", "Sete de copas", "Sete de espadas", "Sete de ouro", "Sete de paus", "Oito de copas", "Oito de espadas", "Oito de ouro", "Oito de paus", "Nove de copas", "Nove de espadas", "Nove de ouro", "Nove de paus", "Dez de copas", "Dez de espadas", "Dez de ouro", "Dez de paus", "Valete de copas", "Valete de espadas", "Valete de ouro", "Valete de paus", "Dama de copas", "Dama de espadas", "Dama de ouro", "Dama de paus", "Rei de copas", "Rei de espadas", "Rei de ouro", "Rei de paus"],
+    BaralhoValorCarta = [1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 9, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10],
     % A ideia é fazer 2 baralhos, um com os nomes da carta, outro com os valores delas, então quando eu quiser a carta da posição 30, eu utilizo pegaElementoLista(30, NomeCarta, BaralhoNomeCarta) e também utilizo (30, ValorCarta, BaralhoValorCarta).
+    
     pegaElementoLista(1, NomeCarta, BaralhoNomeCarta), %Pega o elemento 1 da lista BaralhoNomeCarta
     write(NomeCarta), write(" "),
     pegaElementoLista(1, ValorCarta, BaralhoValorCarta), %Pega o elemento 1 da lista BaralhoValorCarta
     write(ValorCarta),nl,
+    tamanhoLista(BaralhoNomeCarta, Tamanho), %Informa o tamanho da lista passada.
+    write(Tamanho),nl, 
     iniciarPartida().
 
 opcao(2):-
@@ -48,6 +54,10 @@ pegaElementoLista(N, X, [_ | Y]) :-
     pegaElementoLista(M, X, Y),
     N is M+1.    
 
+tamanhoLista([], 0).
+tamanhoLista([_ | R], N) :-
+tamanhoLista(R, N1),
+N is N1+1.
 
     
 main:-
