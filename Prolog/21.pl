37a7dcad52826a1552a5c660722fb93db9d13423
Ícaro Forte <icaro_forte@hotmail.com>
2018-07-25 10:25:59 -0300

    Pegando elementos dos baralhos

--- Prolog/21.pl
+++ Prolog/21.pl
@@ -1,7 +1,10 @@
 %[X,Y|Cauda] = [1,2,3].
 %-pertence(X,[X|Z]).
 %-insere(H, D, [H|D]).
+
+%Lista do tipo [H|T], Onde H é a cabeça e T é o resto da lista.
 :- initialization main.
+
 
 executaMenu():-
     write("---------------------"), nl,
@@ -17,12 +20,18 @@ executaMenu():-
     opcao(OPCAO).
 
 opcao(1):-
-    write("Dificuldade escolhida: Fácil"), nl.
-    %rodadas1jogador(1,0,0).
+    write("Dificuldade escolhida: Fácil"), nl,
+    BaralhoNomeCarta = ["Ás de copas", "Dois de ouro"],
+    BaralhoValorCarta = [1, 2],
+    % A ideia é fazer 2 baralhos, um com os nomes da carta, outro com os valores delas, então quando eu quiser a carta da posição 30, eu utilizo pegaElementoLista(30, NomeCarta, BaralhoNomeCarta) e também utilizo (30, ValorCarta, BaralhoValorCarta).
+    pegaElementoLista(1, NomeCarta, BaralhoNomeCarta), %Pega o elemento 1 da lista BaralhoNomeCarta
+    write(NomeCarta), write(" "),
+    pegaElementoLista(1, ValorCarta, BaralhoValorCarta), %Pega o elemento 1 da lista BaralhoValorCarta
+    write(ValorCarta),nl,
+    iniciarPartida().
 
 opcao(2):-
     write("Dificuldade escolhida: Médio."), nl.
-    %rodadas2jogadores(1, 0, 0).
 
 opcao(3):-
     write("Dificuldade escolhida: Difícil."), nl.
@@ -31,5 +40,15 @@ opcao(_):-
     write("Opção inválida. Selecione outra opção: "),
     executaMenu().
 
+iniciarPartida():-
+    write("Início da partida").
+
+pegaElementoLista(1, X, [X | _]).
+pegaElementoLista(N, X, [_ | Y]) :-
+    pegaElementoLista(M, X, Y),
+    N is M+1.    
+
+
+    
 main:-
     executaMenu().
