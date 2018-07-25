53744ee631ab2545941e7163eaa292e4090092a8
Ícaro Forte <icaro_forte@hotmail.com>
2018-07-25 12:04:46 -0300

    Melhorando exemplo para remover item da lista

--- Prolog/21.pl
+++ Prolog/21.pl
@@ -26,6 +26,7 @@ opcao(1):-
     
     BaralhoNomeCarta = ["As de copas", "As de espadas", "As de ouro", "As de paus", "Dois de copas","Dois de espadas", "Dois de ouro", "Dois de paus", "Tres de copas", "Tres de espadas", "Tres de ouro", "Tres de paus", "Quatro de copas", "Quatro de espadas", "Quatro de ouro", "Quatro de paus", "Cinco de copas", "Cinco de espadas", "Cinco de ouro", "Cinco de paus", "Seis de copas", "Seis de espadas", "Seis de ouro", "Seis de paus", "Sete de copas", "Sete de espadas", "Sete de ouro", "Sete de paus", "Oito de copas", "Oito de espadas", "Oito de ouro", "Oito de paus", "Nove de copas", "Nove de espadas", "Nove de ouro", "Nove de paus", "Dez de copas", "Dez de espadas", "Dez de ouro", "Dez de paus", "Valete de copas", "Valete de espadas", "Valete de ouro", "Valete de paus", "Dama de copas", "Dama de espadas", "Dama de ouro", "Dama de paus", "Rei de copas", "Rei de espadas", "Rei de ouro", "Rei de paus"],
     BaralhoValorCarta = [1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 9, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10],
+    
     % A ideia é fazer 2 baralhos, um com os nomes da carta, outro com os valores delas, então quando eu quiser a carta da posição 30, eu utilizo pegaElementoLista(30, NomeCarta, BaralhoNomeCarta) e também utilizo (30, ValorCarta, BaralhoValorCarta).
     
     pegaElementoLista(1, NomeCarta, BaralhoNomeCarta), %Pega o elemento 1 da lista BaralhoNomeCarta
@@ -34,10 +35,18 @@ opcao(1):-
     write(ValorCarta),nl,
     tamanhoLista(BaralhoNomeCarta, TamanhoBaralhoNomeCarta), %Informa o tamanho da lista passada.
     write("Tamanho Inicial do baralho: "),write(TamanhoBaralhoNomeCarta),nl,
-    remover(NomeCarta, BaralhoNomeCarta, NovoBaralhoNomeCarta),nl, %removendo a carta do baralho
+    
+    %removendo a carta do baralho
+    remover(NomeCarta, BaralhoNomeCarta, NovoBaralhoNomeCarta),nl, 
     write(NovoBaralhoNomeCarta),nl,
     tamanhoLista(NovoBaralhoNomeCarta, NovoTamanhoBaralhoNomeCarta),nl,
     write("Novo tamanho do baralho após remoção: "), write(NovoTamanhoBaralhoNomeCarta),nl,nl,
+    
+    %removendo o valor da carta do baralho
+    remover(ValorCarta, BaralhoValorCarta, NovoBaralhoValorCarta),nl, 
+    tamanhoLista(NovoBaralhoValorCarta, NovoTamanhoBaralhoValorCarta),nl,
+    write("Novo tamanho do baralho de valores após remoção: "), write(NovoTamanhoBaralhoValorCarta),nl,nl,
+    
     iniciarPartida().
 
 opcao(2):-
